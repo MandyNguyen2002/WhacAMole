56022b8a79460a22f3aedceaeaf75b18c833c36d
Mandy Nguyen <mandynguyen0611@gmail.com>
2025-05-23 09:29:04 -1000

    final game basic

--- game.js
+++ game.js
@@ -1,94 +1,137 @@
-function createBoard() {
-  return Array.from({ length: 12 }, (_, i) => ({
-    id: i,
-    hasMole: false,
-  }));
-}
+const Model = (() => {
+  let score = 0;
+  let timeLeft = 30;
+  let board = [];
 
-let board = createBoard();
-console.log(board);
-
-function renderBoard(board) {
-  const imgs = document.querySelectorAll(".hole img");
-  board.forEach((hole, i) => {
-    imgs[i].style.visibility = hole.hasMole ? "visible" : "hidden";
-  });
-}
-
-let moleInterval = setInterval(() => {
-  let moleCount = board.filter((b) => b.hasMole).length;
-
-  if (moleCount < 3) {
-    const emptyHoles = board.filter((b) => !b.hasMole);
-    if (emptyHoles.length > 0) {
-      const randomIdx = Math.floor(Math.random() * emptyHoles.length);
-      const moleId = emptyHoles[randomIdx].id;
-      board[moleId].hasMole = true;
-      renderBoard(board);
-    }
+  function createBoard() {
+    board = Array.from({ length: 12 }, (_, i) => ({
+      id: i,
+      hasMole: false,
+    }));
   }
-}, 1000);
 
-let score = 0;
+  function getBoard() {
+    return board;
+  }
 
-document.querySelectorAll(".hole").forEach((hole) => {
-  hole.addEventListener("click", function () {
-    const holeId = Number(this.getAttribute("data-id"));
+  function getScore() {
+    return score;
+  }
 
-    if (board[holeId].hasMole) {
-      board[holeId].hasMole = false;
-      score++;
-      renderBoard(board);
-      document.getElementById("score").textContent = score;
-    }
-  });
-});
+  function setScore(val) {
+    score = val;
+  }
 
-let timeLeft = 30;
-document.getElementById("timer").textContent = timeLeft;
-let timerInterval = setInterval(() => {
-  timeLeft--;
-  document.getElementById("timer").textContent = timeLeft;
+  function getTime() {
+    return timeLeft;
+  }
 
-  if (timeLeft <= 0) {
+  function setTime(val) {
+    timeLeft = val;
+  }
+
+  return {
+    createBoard,
+    getBoard,
+    getScore,
+    setScore,
+    getTime,
+    setTime,
+  };
+})();
+
+const View = (() => {
+  function renderBoard(board) {
+    const imgs = document.querySelectorAll(".hole img");
+    board.forEach((hole, i) => {
+      imgs[i].style.visibility = hole.hasMole ? "visible" : "hidden";
+    });
+  }
+  function renderScore(score) {
+    document.getElementById("score").textContent = score;
+  }
+  function renderTimer(timeLeft) {
+    document.getElementById("timer").textContent = timeLeft;
+  }
+  function resetView() {
+    renderScore(0);
+    renderTimer(30);
+    renderBoard(
+      Array.from({ length: 12 }, (_, i) => ({ id: i, hasMole: false }))
+    );
+  }
+  return {
+    renderBoard,
+    renderScore,
+    renderTimer,
+    resetView,
+  };
+})();
+
+const Controller = ((model, view) => {
+  let moleInterval = null;
+  let timerInterval = null;
+
+  function startGame() {
     clearInterval(timerInterval);
     clearInterval(moleInterval);
-    alert("Time is Over!");
-  }
-}, 1000);
 
-document.getElementById("start-btn").addEventListener("click", function () {
-  clearInterval(timerInterval);
-  clearInterval(moleInterval);
+    model.createBoard();
+    model.setScore(0);
+    model.setTime(30);
 
-  board = createBoard();
-  score = 0;
-  timeLeft = 30;
+    view.resetView();
 
-  document.getElementById("score").textContent = score;
-  document.getElementById("timer").textContent = timeLeft;
-  renderBoard(board);
-
-  moleInterval = setInterval(() => {
-    let moleCount = board.filter((b) => b.hasMole).length;
-    if (moleCount < 3) {
-      const emptyHoles = board.filter((b) => !b.hasMole);
-      if (emptyHoles.length > 0) {
-        const randomIdx = Math.floor(Math.random() * emptyHoles.length);
-        const moleId = emptyHoles[randomIdx].id;
-        board[moleId].hasMole = true;
-        renderBoard(board);
+    timerInterval = setInterval(() => {
+      let t = model.getTime() - 1;
+      model.setTime(t);
+      view.renderTimer(t);
+      if (t <= 0) {
+        clearInterval(timerInterval);
+        clearInterval(moleInterval);
+        alert("Time is Over!");
       }
-    }
-  }, 1000);
+    }, 1000);
 
-  timerInterval = setInterval(() => {
-    timeLeft--;
-    document.getElementById("timer").textContent = timeLeft;
-    if (timeLeft <= 0) {
-      clearInterval(timerInterval);
-      clearInterval(moleInterval);
-      alert("Time is Over!");
-    }
-  }, 1000);
-});
+    moleInterval = setInterval(() => {
+      const board = model.getBoard();
+      let moleCount = board.filter((b) => b.hasMole).length;
+      if (moleCount < 3) {
+        const emptyHoles = board.filter((b) => !b.hasMole);
+        if (emptyHoles.length > 0) {
+          const randomIdx = Math.floor(Math.random() * emptyHoles.length);
+          const moleId = emptyHoles[randomIdx].id;
+          board[moleId].hasMole = true;
+          view.renderBoard(board);
+        }
+      }
+    }, 1000);
+  }
+
+  function addHoleListeners() {
+    document.querySelectorAll(".hole").forEach((hole) => {
+      hole.addEventListener("click", function () {
+        const holeId = Number(this.getAttribute("data-id"));
+        const board = model.getBoard();
+        if (board[holeId].hasMole) {
+          board[holeId].hasMole = false;
+          model.setScore(model.getScore() + 1);
+          view.renderScore(model.getScore());
+          view.renderBoard(board);
+        }
+      });
+    });
+  }
+
+  document.getElementById("start-btn").addEventListener("click", startGame);
+
+  const init = () => {
+    model.createBoard();
+    view.resetView();
+    addHoleListeners();
+  };
+
+  return { init };
+})(Model, View);
+
+Controller.init();
